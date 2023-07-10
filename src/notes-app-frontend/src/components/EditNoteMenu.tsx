e379b93d2a18402f89f240c212d81822c1ac63d6
Octavio Zenil <octaviozenil@gmail.com>
2023-07-09 21:26:12 -0700

    SyncMessage timeout implemented

--- src/notes-app-frontend/src/components/EditNoteMenu.tsx
+++ src/notes-app-frontend/src/components/EditNoteMenu.tsx
@@ -1,22 +1,64 @@
 import { NoteProps } from "./Note"
 import { colors } from "../constants"
-import { SetStateAction} from "react"
+import { SetStateAction, useEffect, useRef} from "react"
 import closeIcon from '../assets/close.svg'
+import syncIcon from '../assets/sync.svg'
+
+export type SyncDataHandler = (changes: Partial<NoteProps>) => void
 
 interface EditNoteMenuProps {
     noteProperties?: NoteProps
     setIsActive: React.Dispatch<SetStateAction<boolean>>
+    onSyncData: SyncDataHandler
 }
 
 
 
-export const EditNoteMenu = ({noteProperties = undefined, setIsActive}:EditNoteMenuProps) => {
+export const EditNoteMenu = ({noteProperties = undefined, setIsActive, onSyncData}:EditNoteMenuProps) => {
+
+    const title = useRef<HTMLInputElement>(null)
+    const description = useRef<HTMLTextAreaElement>(null)
+
+    const syncOnEnter = (event:KeyboardEvent) => {
+        if (event.key == 'Enter'){
+            handleSyncData()
+
+        }
+    }
+    
+    useEffect(() => {
+        window.addEventListener('keypress',syncOnEnter)
+
+        return () => {
+            window.removeEventListener('keypress',syncOnEnter)
+        }
+    },[])
+    
+    const handleSyncData = () => {
+
+        if (!noteProperties)
+            return
+        
+        if ( !title.current || !description.current)
+            throw new Error('No title or description element')
+        
+        let changes: Partial<NoteProps> = {id: noteProperties.id}
+
+        
+        if (title.current.value != noteProperties.name)
+            changes.name = title.current.value
+
+        if (description.current.innerText != noteProperties.content)
+            changes.content = description.current.value
+        
+        onSyncData(changes)
+    }
 
     
     if (!noteProperties)
         return <></>
 
-    console.log(colors.find((color) => color.includes(noteProperties.color)))
+    // console.log(colors.find((color) => color.includes(noteProperties.color)))
         return (
             <>
                 <section
@@ -27,14 +69,15 @@ export const EditNoteMenu = ({noteProperties = undefined, setIsActive}:EditNoteM
                     className={`${colors.find((color) => color.includes(noteProperties.color))} z-20 h-3/4 aspect-video absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-xl p-10 flex flex-col gap-12`}
                 >
                     <button 
-                        className="w-5 hover:w-7 hover:translate-x-1 hover:-translate-y-1  transition-all absolute right-5 top-5 outline-none"
+                        className="w-5 hover:scale-150 transition-all absolute right-5 top-5 outline-none"
                         onClick={() => setIsActive(false)}
                     >
                         <img src={closeIcon} alt="" />
                     </button>
                     <input type="text"
+                        ref={title}
                         placeholder="New Title"
-                        value={noteProperties.name}
+                        defaultValue={noteProperties.name}
                         className="bg-inherit border-b-2 border-b-black outline-none text-3xl placeholder:text-black placeholder:opacity-60 w-full"
                     />
 
@@ -45,11 +88,23 @@ export const EditNoteMenu = ({noteProperties = undefined, setIsActive}:EditNoteM
                             Description
                         </label>
                         <textarea
+                            ref={description}
                             className="bg-inherit border-2 border-black rounded-xl resize-none outline-none flex-1 p-5"
+                            defaultValue={noteProperties.content}
                         >
-                            {noteProperties.content}
                         </textarea>
                     </div>
+                    <div
+                        className="flex w-full"
+                    >
+                        <button 
+                        className="ml-auto w-6   outline-none"
+                        onClick={handleSyncData}
+                    >
+                        <img src={syncIcon} alt="" className="w-6 hover:scale-150  transition-all" />
+                    </button>
+                    </div>
+
                 </section>
             </>
         )
--- src/notes-app-frontend/src/components/NotesBoard.tsx
+++ src/notes-app-frontend/src/components/NotesBoard.tsx
@@ -1,6 +1,7 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Note, NoteProps } from "./Note"
 import { EditNoteMenu } from "./EditNoteMenu"
+import { SyncMessage } from "./SyncMessage"
 
 
 interface props {
@@ -12,7 +13,19 @@ export const NotesBoard = ({tailwindStyles,notesArray}:props) => {
 
     const [notes, setNotes] = useState(notesArray)
     const [editing, setEditing] = useState(false)
+    const [showingSyncMessage, setShowingSyncMessage] = useState(false)
+    const [isAccomplishedSync, setAccomplishedSync] = useState(false)
     let activeNote = useRef<NoteProps | undefined>(undefined)
+
+    useEffect(()=>{
+        let showingSyncMessageTimeOut = setTimeout(() => {
+            setShowingSyncMessage(false)
+        },3000)
+        
+        return () => {
+            clearTimeout(showingSyncMessageTimeOut)
+        }
+    },[showingSyncMessage])
 
     const findIndexNoteById = (noteId:number):number => {
         let index = notes.findIndex((note) => note.id == noteId)
@@ -49,11 +62,23 @@ export const NotesBoard = ({tailwindStyles,notesArray}:props) => {
         
     }
 
+    const handleOnSyncData = (changes:Partial<NoteProps>) => {
+        console.log(changes)
+        setAccomplishedSync(Boolean(Math.floor(Math.random()*2)))
+        setShowingSyncMessage(true)
+    }
+
+    const handleSyncMessageDissapear = () => {
+        activeNote.current = undefined
+        setShowingSyncMessage(false)
+    }
+
     return (
         <section className={`${tailwindStyles} pt-10 px-32 grid grid-cols-[repeat(auto-fit,minmax(12rem,1fr))] grid-rows-[repeat(auto-fit,minmax(100px,12rem))] gap-x-24 gap-y-8 overflow-y-scroll`}>
             {
                 editing && 
                 <EditNoteMenu
+                    onSyncData={handleOnSyncData}
                     noteProperties={activeNote.current}
                     setIsActive={setEditing}
                 />
@@ -61,12 +86,20 @@ export const NotesBoard = ({tailwindStyles,notesArray}:props) => {
             
             {...notes.map((note) =>
                 <Note
+            
                     key={note.id}
                     noteProperties={note}
                     onEdition={handleEdition}
                     onFavoriteToggle={handleFavoriteToggle}
                 />
             )}
+
+            <SyncMessage
+                note={activeNote.current}
+                accomplished = {isAccomplishedSync}
+                showing = {showingSyncMessage}
+                disappear={handleSyncMessageDissapear}
+            />
         </section>
     )
 }
